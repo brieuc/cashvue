c503071fcf26326b5e1a6a516300d688f60d7e44
Brieuc GORIN <brieuc.gorin@gmail.com>
2026-09-09 11:03:56 +0200

    versionning

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,17 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { readFileSync } from 'node:fs'
+
+const { version } = JSON.parse(readFileSync(new URL('./package.json', import.meta.url), 'utf-8'))
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/cashtag/',
   plugins: [vue(), vueDevTools()],
   define: {
-    '__VUE_PROD_DEVTOOLS__': false
+    '__VUE_PROD_DEVTOOLS__': false,
+    '__APP_VERSION__': JSON.stringify(version)
   },
   resolve: {
     alias: {
